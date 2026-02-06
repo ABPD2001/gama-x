c73313a5fa8e931c0bdcbb7a5118467d49609b0e
abpd2001 <a.pouretemadi@gmail.com>
2026-02-06 21:27:38 +0330

    just persoanl changes...

--- utils/number.cpp
+++ utils/number.cpp
@@ -4,7 +4,8 @@ uint32_t to_uint32(string text){
         uint32_t output = 0;
 
         for(uint8_t i = 0; i<text.length(); i++){
-                output+=(text[i]-48)*pow(10,i);
+                cout<<text[i]-48<<"\n";
+		output+=(text[i]-48)*pow(10,i);
         }
 
         return output;
--- utils/number.hpp
+++ utils/number.hpp
@@ -3,10 +3,12 @@
 #include <string>
 #include <cstdint>
 #include <cmath>
+#include <iostream>
 
 using std::string;
 using std::uint32_t;
 using std::pow;
+using std::cout;
 
 uint32_t to_uint32(string text);
 #endif
